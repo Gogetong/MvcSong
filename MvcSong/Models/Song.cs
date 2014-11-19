64495b54ec7d96be05a68b52be3a11ea4a2f8f8a
Gogetong <gogetong@hotmail.com>
2014-11-19 11:51:15 +0700

    FirstCommit

---
+++ MvcSong/Models/Song.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcSong.Models
+{
+    public class Song
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class SongDBContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+    }
+}
